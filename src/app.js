cbe5c87aed3c5530360e0eb3f3bf0b2d114c19d5
João <joaokz@outlook.com>
2025-01-06 15:44:47 -0300

    CRUD finished

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const livros = [
+let livros = [
     {
         id: 1,
         name: "Can't Hurt Me: Master Your Mind and Defy the Odds"
@@ -13,11 +13,45 @@ const livros = [
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello World! Minha Primeira API Node.js!")
 })
 
 app.get("/livros", (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get("/livros/:id", (req, res) => {
+    const bookId = req.params.id
+    const searchedBook = livros.filter((livro) => livro.id === Number(bookId))
+    res.status(200).json(searchedBook)
+})
+
+app.post("/livros", (req, res) => {
+    const newBook = req.body
+    livros.push(newBook)
+    res.status(201).end("Livro cadastrado com sucesso!")
+})
+
+app.put("/livros/:id", (req, res) => {
+    const bookId = req.params.id
+    livros = livros.map((livro) => {
+        if(livro.id === Number(bookId)){
+            return {
+                ...livro,
+                name: req.body.name
+            }
+        }
+        return livro
+    })
+    res.status(200).json(livros)
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const bookId = Number(req.params.id)
+    livros = livros.filter((livro) => livro.id !== bookId)
     res.status(200).json(livros)
 })
 
